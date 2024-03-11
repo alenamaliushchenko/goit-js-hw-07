c91949dd1af4c68f5f3029a6674f5a28a4c20c78
Alena Maliushchenko <alenamaliushchenko@gmail.com>
2024-03-11 15:57:40 +0200

    improved task 5

--- js/task-5.js
+++ js/task-5.js
@@ -2,13 +2,6 @@
 // інлайн - стиль по кліку на button.change - color і задає це значення кольору
 // текстовим вмістом для span.color
 
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
-
-const widget = document.querySelector('.widget');
 const buttonChange = document.querySelector('.change-color');
 const colorSpan = document.querySelector('.color');
 buttonChange.addEventListener('click', function () {
